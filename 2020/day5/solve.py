95620fe47e8efab25615f7e38bebcb02ae0227df
Zac Oler <zac.oler@gmail.com>
2023-07-07 20:51:43 -0500

    2020.04 and 2020.05

---
+++ 2020/day5/solve.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+filename = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+
+with open(filename) as fh:
+    lines = [line.strip() for line in fh.readlines()]
+
+
+def parseit(seat: str):
+    rows = list(range(128))
+    cols = list(range(8))
+    for i in seat[:7]:
+        if i == 'F': rows = rows[:len(rows)//2]
+        elif i == 'B': rows = rows[len(rows)//2:]
+        else: print('fail:', i, seat); exit(1)
+    for i in seat[7:]:
+        if i == 'L': cols = cols[:len(cols)//2]
+        elif i == 'R': cols = cols[len(cols)//2:]
+        else: print('fail:', i, seat); exit(1)
+    row, col = rows[0], cols[0]
+    #print(row, col)
+    return row * 8 + col
+
+seat_ids = [parseit(l) for l in lines]
+print(seat_ids)
+print(max(seat_ids))
