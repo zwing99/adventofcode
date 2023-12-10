74a8ebf44a9349407d3dae6620a914b8729975a3
Zac Oler <zac.oler@gmail.com>
2023-12-10 12:30:11 -0600

    old solutions

---
+++ 2021/11/solve2.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import sys
+filename = sys.argv[1] if len(sys.argv) > 1 else 'input.txt'
+
+with open(filename) as fh:
+    lines = [[int(x) for x in line.strip()] for line in fh.readlines()]
+
+height = len(lines)
+width = len(lines[0])
+all_directs = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (-1, 1), (1, -1)]
+
+def pop_it(x, y):
+    pop_more = set()
+    for m,n in all_directs:
+        a = x + m
+        b = y + n
+        if 0 <= a and a < width and 0 <= b and b < height:
+            lines[b][a] += 1
+            if lines[b][a] >= 10:
+                #print (a, b)
+                pop_more.add((a,b))
+    return pop_more
+
+
+f = []
+z = 0
+while True:
+    z += 1
+    for j in range(height):
+        for i in range(width):
+            lines[j][i] += 1
+
+    to_pop = set()
+    for j in range(height):
+        for i in range(width):
+            if lines[j][i] >= 10:
+                to_pop.add((i, j))
+
+    popped = set()
+    while len(to_pop) > 0:
+        x, y = to_pop.pop()
+        if (x, y) in popped:
+            print('huh?')
+            continue
+        popped.add((x, y))
+        moar = pop_it(x, y)
+        to_pop.update(moar - popped)
+
+    flashes = 0
+    for j in range(height):
+        for i in range(width):
+            if lines[j][i] >= 10:
+                lines[j][i] = 0
+                flashes += 1
+    f.append(flashes)
+    #print (_ + 1, sum(f))
+    #for line in lines:
+    #    print(' '.join([str(x) for x in line]))
+    #print("----------------------------")
+    
+    #if _ >= 2:
+    #    break
+    
+    if flashes == 100:
+        break
+
+print(z)
+    
+
+
