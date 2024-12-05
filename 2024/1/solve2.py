d083d4cfce8048a21e295ba1e8d1c47a597adffc
Zac Oler <zac.oler@gmail.com>
2024-12-04 19:37:44 -0600

    2024.1

---
+++ 2024/1/solve2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+
+with open(filename) as fh:
+    lines = [line.strip().split() for line in fh.readlines()]
+
+li1 = sorted([int(l[0]) for l in lines])
+li2 = sorted([int(l[1]) for l in lines])
+
+print(sum(map(lambda x: li2.count(x) * x, li1)))
