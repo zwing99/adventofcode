124fbd19ab87e3e625e739cf54cb618970dcb792
Zac Oler <zac.oler@gmail.com>
2025-12-04 20:12:55 -0600

    2025.4

---
+++ 2025/4/solve.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import itertools
+from re import L
+from statistics import LinearRegression
+import sys
+
+filename = sys.argv[1] if len(sys.argv) > 1 else "input.txt"
+
+with open(filename) as fh:
+    lines = [line.strip() for line in fh.readlines()]
+
+
+x_min = 0
+x_max = len(lines[0])
+y_min = 0
+y_max = len(lines)
+dirs: set[tuple[int, int]] = set(itertools.product([-1, 0, 1], [-1, 0, 1]))
+dirs.remove((0, 0))
+
+
+count = 0
+for y in range(y_max):
+    for x in range(x_max):
+        if lines[y][x] == "@":
+            total = 0
+            for d in dirs:
+                xn, yn = x + d[0], y + d[1]
+                if xn >= x_min and xn < x_max and yn >= y_min and yn < y_max:
+                    # print(yn, xn)
+                    if lines[yn][xn] == "@":
+                        total += 1
+            if total < 4:
+                count += 1
+
+print(count)
